a4760639b13a582c735e1024c0ff4c381deb6794
Somesh Jayant Deshmukh <somesh.deshmukh@yash.com>
2022-10-10 19:42:45 +0530

    new

---
+++ db/dbConnect.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+async function dbConnect() {
+  
+  mongoose
+    .connect(
+        process.env.DB_URL,
+      {
+      
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        
+      }
+    )
+    .then(() => {
+      console.log("Successfully connected to MongoDB Atlas!");
+    })
+    .catch((error) => {
+      console.log("Unable to connect to MongoDB Atlas!");
+      console.error(error);
+    });
+}
+
+module.exports = dbConnect;
